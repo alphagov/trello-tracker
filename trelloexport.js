50dc4a422a4802e411e6f640d145e1d7827fc86a
Pablo Manrubia <pmanrubia@gmail.com>
2016-08-04 12:03:26 +0100

    Move code related to configure the menu item
    This is done in preparation to extract an object responsible with
     creating the menu configuration.

--- trelloexport.js
+++ trelloexport.js
@@ -18,8 +18,6 @@ var $,
     saveAs;
 
 
-// Variables
-var addInterval;
 
 window.URL = window.webkitURL || window.URL;
 
@@ -180,40 +178,44 @@ function createExcelExport() {
 }
 
 
-// Add a Export Excel button to the DOM and trigger export if clicked
-function addExportLink() {
-  "use strict";
-  //alert('add');
-
-  var $js_btn = $('a.js-export-json'); // Export JSON link
-
-  // See if our Export Excel is already there
-  if ($('.pop-over-list').find('.js-export-excel').length) {
-    clearInterval(addInterval);
-    return;
-  }
-
-  // The new link/button
-  if ($js_btn.length) {
-    var $excel_btn = $('<a>')
-        .attr({
-          'class': 'js-export-excel',
-          'href': '#',
-          'target': '_blank',
-          'title': 'Open downloaded file with Excel'
-        });
-    $excel_btn.text('Export Excel')
-        .click(createExcelExport)
-        .insertAfter($js_btn.parent())
-        .wrap(document.createElement("li"));
-
-  }
-}
-
-
 // on DOM load
 $(function () {
   "use strict";
+
+  // Variables
+  var addInterval;
+
+// Add a Export Excel button to the DOM and trigger export if clicked
+  function addExportLink() {
+    "use strict";
+    //alert('add');
+
+    var $js_btn = $('a.js-export-json'); // Export JSON link
+
+    // See if our Export Excel is already there
+    if ($('.pop-over-list').find('.js-export-excel').length) {
+      clearInterval(addInterval);
+      return;
+    }
+
+    // The new link/button
+    if ($js_btn.length) {
+      var $excel_btn = $('<a>')
+          .attr({
+            'class': 'js-export-excel',
+            'href': '#',
+            'target': '_blank',
+            'title': 'Open downloaded file with Excel5'
+          });
+      $excel_btn.text('Export Excel')
+          .click(createExcelExport)
+          .insertAfter($js_btn.parent())
+          .wrap(document.createElement("li"));
+    }
+  }
+
+
+
   // Look for clicks on the .js-share class, which is
   // the "Share, Print, Export..." link on the board header option list
   $(document).on('mouseup', ".js-share", function () {
