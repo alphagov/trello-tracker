35caa562859988046b7a5a1161ab42c91397a55d
Pablo Manrubia <pmanrubia@gmail.com>
2016-08-08 16:52:26 +0100

    Remove list column
    We don’t need to iterate through the lists because we are only going
    to use one.

--- trelloexport.js
+++ trelloexport.js
@@ -18,49 +18,41 @@ function createExcelExport() {
   $.getJSON(apiURL, function (data) {
 
     var spreadSheet = new SpreadSheet(data.name);
-    spreadSheet.addHeader(['List', 'Title', 'Description', 'Due', 'Labels', 'Card #', 'Card URL']);
+    spreadSheet.addHeader(['Title', 'Description', 'Due', 'Labels', 'Card #', 'Card URL']);
 
-    // This iterates through each list and builds the dataset
-    $.each(data.lists, function (key, list) {
-      var list_id = list.id;
+    // Iterate through each card and transform data as needed
+    $.each(data.cards, function (i, card) {
 
-      // Iterate through each card and transform data as needed
-      $.each(data.cards, function (i, card) {
-        if (card.idList === list_id) {
-
-          var labels = [];
-          $.each(card.labels, function (i, label) {
-            if (label.name) {
-              labels.push(label.name);
-            } else {
-              labels.push(label.color);
-            }
-
-          });
-
-
-          // Need to set dates to the Date type so xlsx.js sets the right datatype
-          var due = card.due || '';
-          if (due !== '') {
-            due = new Date(due);
-          }
-
-          var rowData = [
-            list.name,
-            card.name,
-            card.desc,
-            due,
-            labels.toString(),
-            card.idShort,
-            card.shortUrl
-          ];
-
-          // Writes all closed items to the Archived tab
-          if (!list.closed && !card.closed) {
-            spreadSheet.addRow(rowData);
-          }
+      var labels = [];
+      $.each(card.labels, function (i, label) {
+        if (label.name) {
+          labels.push(label.name);
+        } else {
+          labels.push(label.color);
         }
+
       });
+
+
+      // Need to set dates to the Date type so xlsx.js sets the right datatype
+      var due = card.due || '';
+      if (due !== '') {
+        due = new Date(due);
+      }
+
+      var rowData = [
+        card.name,
+        card.desc,
+        due,
+        labels.toString(),
+        card.idShort,
+        card.shortUrl
+      ];
+
+      // Writes all closed items to the Archived tab
+      if (!card.closed) {
+        spreadSheet.addRow(rowData);
+      }
     });
 
     spreadSheet.export();
@@ -75,3 +67,11 @@ $(function () {
 
   new Menu().init();
 });
+
+//var rows = transformCardsToRows(data);
+//
+//var spreadSheet = new SpreadSheet(data.name);
+//spreadSheet.addHeader(['List', 'Title', 'Description', 'Due', 'Labels', 'Card #', 'Card URL']);
+//spreadSheet.addRows(rows);
+//spreadSheet.exportToFile();
+//
