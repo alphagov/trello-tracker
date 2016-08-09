6883b5459a98d09679c74e6b85849cc967a611db
Pablo Manrubia <pmanrubia@gmail.com>
2016-08-09 17:28:24 +0100

    Add test to #addRows

--- spec/spreadSheetSpec.js
+++ spec/spreadSheetSpec.js
@@ -9,4 +9,17 @@ describe("SpreadSheet", function () {
     });
   });
 
+  describe("#addRows", function() {
+    it("Add rows to the worksheet", function () {
+      var spreadSheet = new SpreadSheet('a title');
+      spreadSheet.addHeader(['header1', 'header2']);
+      var rows = [
+          ['1', '2'],
+          ['3', '4']
+      ];
+      spreadSheet.addRows(rows);
+      expect(spreadSheet.worksheet.data[1]).toEqual(['1','2']);
+      expect(spreadSheet.worksheet.data[2]).toEqual(['3','4']);
+    })
+  })
 });
