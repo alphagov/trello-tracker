123b974133178f09dc9d365de178c86427a226e3
Pablo Manrubia <pmanrubia@gmail.com>
2016-08-09 13:24:50 +0100

    No need to close the popup menu.

--- trelloexport.js
+++ trelloexport.js
@@ -3,7 +3,6 @@ window.URL = window.webkitURL || window.URL;
 function createExcelExport() {
   "use strict";
   new Factcheck().createExport();
-  $("a.pop-over-header-close-btn")[0].click();
 }
 
 
