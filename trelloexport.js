f3b06eb6497e38b324d832507a76c173ecb5421c
Pablo Manrubia <pmanrubia@gmail.com>
2016-08-09 13:03:02 +0100

    Move call to get all Cards to the Factcheck class

--- trelloexport.js
+++ trelloexport.js
@@ -2,17 +2,7 @@ window.URL = window.webkitURL || window.URL;
 
 function createExcelExport() {
   "use strict";
-
-  new TrelloAPI().getAllCards(function (data, listName) {
-
-    var spreadSheet = new SpreadSheet(listName);
-    spreadSheet.addHeader(['Title', 'Description', 'Due', 'Labels', 'Card #', 'Card URL']);
-
-    var rows = new Factcheck(data).transformRows();
-    spreadSheet.addRows(rows);
-
-    spreadSheet.export();
-  });
+  new Factcheck().createExport();
   $("a.pop-over-header-close-btn")[0].click();
 }
 
