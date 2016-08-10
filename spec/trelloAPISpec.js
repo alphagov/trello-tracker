26405621ceebcc8e13e3fa2d19cab4eb591d62a9
Pablo Manrubia <pmanrubia@gmail.com>
2016-08-10 15:18:30 +0100

    Test we get the board name when retrieving all cards
    Changed to use an `ajax` request rather than getJSON to call the Trello API.
    Using `ajax` allows us to mock the promise (done function) and “resolve” 
    what we expect to receive from Trello. This allows us to test what is
    being passed to the callback.

--- spec/trelloAPISpec.js
+++ spec/trelloAPISpec.js
@@ -4,15 +4,37 @@ describe("TrelloAPI", function () {
     it("requests cards for the current Trello board", function () {
       var trelloAPI = new TrelloAPI();
 
-      spyOn($, 'getJSON');
+      var deferred = $.Deferred();
+      deferred.resolve({});
+
+      spyOn($, 'ajax').and.returnValue(deferred.promise());
       spyOn(trelloAPI, 'currentBrowserLocation').and.returnValue('/b/the_board_id/the_board_name');
 
       trelloAPI.getAllCards(function (data) {
         //noop
       });
 
-      var call = $.getJSON.calls.first();
+      var call = $.ajax.calls.first();
       expect(call.args[0]).toMatch(/the_board_id/);
     });
+
+    it("the callback is being passed the board name", function () {
+      var trelloAPI = new TrelloAPI();
+
+      var deferred = $.Deferred();
+      var json = {
+        name: 'the board name',
+        cards: []
+      };
+      spyOn($, 'ajax').and.returnValue(deferred.promise());
+      deferred.resolve(json);
+
+      var name;
+      trelloAPI.getAllCards(function (cards, boardName) {
+        name = boardName;
+      });
+
+      expect(name).toEqual('the board name');
+    })
   });
 });
