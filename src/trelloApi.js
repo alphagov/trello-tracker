b8da95b2177472e571ba97e4aa24d5204f4d2d18
Pablo Manrubia <pmanrubia@gmail.com>
2016-08-18 16:05:09 +0100

    Filter actions that represent a change of list
    It also increases the number of actions to 1000, as before, with 50 we
    were not able to calculate the time on the list

--- src/trelloApi.js
+++ src/trelloApi.js
@@ -1,7 +1,7 @@
 TrelloAPI = {
   getAllCards: function (callback) {
     var boardId = this._getBoardId()
-    var apiURL = "https://trello.com/1/boards/" + boardId + "?lists=open&cards=open&card_attachments=true&actions=updateCard&desc%2Cdue%2Clabels%2Cname%2CshortUrl%2CshortLink&desc%2CdescData%2Curl&fields=name%2CshortLink%2CshortUrl%2Curl%2Cdesc";
+    var apiURL = "https://trello.com/1/boards/" + boardId + "?lists=open&cards=open&card_attachments=true&actions_limit=1000&actions=updateCard:idList&desc%2Cdue%2Clabels%2Cname%2CshortUrl%2CshortLink&desc%2CdescData%2Curl&fields=name%2CshortLink%2CshortUrl%2Curl%2Cdesc";
 
     $.ajax(apiURL).done(function (data) {
       callback(data.cards, data.name, data.actions);
