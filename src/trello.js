e2e6d8c32cd69bc0d0af3e3049f32543bf3afe8f
Pablo Manrubia <pmanrubia@gmail.com>
2016-08-19 14:30:40 +0100

    Dry-up Trello

--- src/trello.js
+++ src/trello.js
@@ -1,4 +1,5 @@
-Trello = {
+var Trello = {
+
   getAllCards: function (callback) {
     var boardId = this._getBoardId()
     var apiURL = "https://trello.com/1/boards/" + boardId + "?lists=open&cards=open&card_attachments=true&actions_limit=1000&actions=updateCard:idList&desc%2Cdue%2Clabels%2Cname%2CshortUrl%2CshortLink&desc%2CdescData%2Curl&fields=name%2CshortLink%2CshortUrl%2Curl%2Cdesc";
@@ -17,54 +18,50 @@ Trello = {
   },
 
   findZendeskTicketID: function (card) {
-    if (card.attachments.length > 0) {
-      for (var i = 0; i < card.attachments.length; i++) {
-        var attachment = card.attachments[i];
-        var match = attachment.url.match(/govuk.zendesk.com\/agent\/#\/tickets\/(.+)/);
-        if (match)
-          return match[1];
-      }
+    for (var i = 0; i < card.attachments.length; i++) {
+      var attachment = card.attachments[i];
+      var match = attachment.url.match(/govuk.zendesk.com\/agent\/#\/tickets\/(.+)/);
+      if (match)
+        return match[1];
     }
+
     return '-';
   },
 
   findZendeskTicketURL: function (card) {
-    if (card.attachments.length > 0) {
-      for (var i = 0; i < card.attachments.length; i++) {
-        var attachment = card.attachments[i];
-        if (attachment.url.match(/govuk.zendesk.com/))
-          return attachment.url;
-      }
+    for (var i = 0; i < card.attachments.length; i++) {
+      var attachment = card.attachments[i];
+      if (attachment.url.match(/govuk.zendesk.com/))
+        return attachment.url;
     }
+
     return '-';
   },
 
-
   findPublishingURL: function (card) {
-    if (card.attachments.length > 0) {
-      for (var i = 0; i < card.attachments.length; i++) {
-        var attachment = card.attachments[i];
-        if (attachment.url.match(/publishing.service.gov.uk/))
-          return attachment.url;
-      }
+    for (var i = 0; i < card.attachments.length; i++) {
+      var attachment = card.attachments[i];
+      if (attachment.url.match(/publishing.service.gov.uk/))
+        return attachment.url;
     }
+
     return '-';
   },
 
   findCardStatusDays: function (card, actions) {
-    if (actions.length > 0) {
-      for (var i = 0; i < actions.length; i++) {
-        var action = actions[i];
-        if (action.data.card.id === card.id) {
-          if (action.data.listAfter) {
-            var today = moment();
-            var statusDate = moment(action.date);
+    for (var i = 0; i < actions.length; i++) {
+      var action = actions[i],
+          actionCardId = action.data.card.id;
+      if (actionCardId === card.id) {
+        if (action.data.listAfter) {
+          var today = moment();
+          var statusDate = moment(action.date);
 
-            return today.diff(statusDate, "days");
-          }
+          return today.diff(statusDate, "days");
         }
       }
     }
+
     return '-';
   },
 
